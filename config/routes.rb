c3505954ab9c0c5dbadee69c34453f64b5b7feb7
tansen14 <s1B101402816@gmail.com>
2017-03-15 10:34:51 +0000

    課題解き終わり!(^^)!

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :articles
+
+  get '/users' => 'users#index'
+	get '/users/:id' => 'users#show'
+
   root 'articles#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
